3f5201edb5f095aa34eff08ee7a96812fc0faf90
aarburu <aarburuzu25bj@ikzubirimanteo.com>
2025-10-29 14:27:59 +0100

    HealthSystemSingleton

---
+++ Assets/Scripts/General/Singleton.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    private static object _lock = new object();
+    private static bool _applicationIsQuitting = false;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_applicationIsQuitting)
+            {
+                Debug.LogWarning($"[Singleton] Instance '{typeof(T)}' already destroyed on application quit. Won't create again.");
+                return null;
+            }
+
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = (T)FindFirstObjectByType(typeof(T));
+
+                    if (FindObjectsByType(typeof(T), FindObjectsSortMode.None).Length > 1)
+                    {
+                        Debug.LogError($"[Singleton] Multiple instances of singleton {typeof(T)} detected!");
+                        return _instance;
+                    }
+
+                    if (_instance == null)
+                    {
+                        GameObject singletonObject = new GameObject();
+                        _instance = singletonObject.AddComponent<T>();
+                        singletonObject.name = $"{typeof(T)} (Singleton)";
+                        DontDestroyOnLoad(singletonObject);
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        _applicationIsQuitting = true;
+    }
+}
---
+++ Assets/Scripts/Systems/HealthSystem.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Xml;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HealthSystem : Singleton<HealthSystem>
+{
+    // Referencias UI. TODO:: Sacar a un HealthSystem para separar la lógica.
+    [field: SerializeField] TMP_Text HealthUIReference;
+
+    [field: SerializeField] private Color colourA = Color.red;
+    [field: SerializeField] private Color colourB = Color.white;
+    [field: SerializeField] private Color DeadColour = Color.gray;
+
+    [field: SerializeField] private float interval = 0.5f;
+    private Coroutine flashRoutine;
+
+
+    static int MAX_INITIAL_HEALTH = 3;
+    int CurrentMaxHealth { get; set; } = MAX_INITIAL_HEALTH;
+    int CurrentHealth { get; set; } = MAX_INITIAL_HEALTH;
+    public bool IsDead => CurrentHealth == 0;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    public void ReceiveDamage()
+    {
+        this.CurrentHealth--;
+        UpdateUI();
+    }
+
+    public void ReceiveLethalDamage()
+    {
+        this.CurrentHealth = 0;
+        UpdateUI();
+    }
+
+
+    public void ReceiveHealing()
+    {
+        if (this.CurrentHealth < this.CurrentMaxHealth)
+        {
+            this.CurrentHealth++;
+            UpdateUI();
+        }
+    }
+
+    private void UpdateUI()
+    {
+        HealthUIReference.text = $"HP: {this.CurrentHealth}";
+        ActivateLowHPEffect();
+    }
+
+    private void ActivateLowHPEffect()
+    {
+        if (CurrentHealth == 1)
+        {
+            StartFlashing();
+        }
+        else if (CurrentHealth > 1)
+        {
+            StopFlashing();
+        }
+        else
+        {
+            StopFlashing();
+            HealthUIReference.color = DeadColour;
+        }
+    }
+
+    public void StartFlashing()
+    {
+        if (flashRoutine == null)
+            flashRoutine = StartCoroutine(FlashText());
+    }
+
+    public void StopFlashing()
+    {
+        if (flashRoutine != null)
+        {
+            StopCoroutine(flashRoutine);
+            flashRoutine = null;
+            HealthUIReference.color = colourB;
+        }
+    }
+
+    private IEnumerator FlashText()
+    {
+        bool toggle = false;
+        while (true)
+        {
+            HealthUIReference.color = toggle ? colourA : colourB;
+            toggle = !toggle;
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
